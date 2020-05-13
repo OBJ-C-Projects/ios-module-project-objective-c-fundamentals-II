cd2b1d48afef010b8d3f2327051a3baa1ffdd6ac
fritzgt <tuanis@live.com>
2020-05-13 09:29:13 +0300

    init properties

--- TimeTracker/Model/FGTTimedTask.h
+++ TimeTracker/Model/FGTTimedTask.h
@@ -12,6 +12,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FGTTimedTask : NSObject
 
+@property (nonatomic) NSString *client;
+@property (nonatomic) NSString *summary;
+@property (nonatomic) NSNumber *rate;
+@property (nonatomic) NSNumber *hours;
+@property (nonatomic, readonly) NSNumber *total;
+
+- (instancetype)initWithClient:(NSString *)client
+                       summary:(NSString *)summary
+                          rate:(NSNumber *)rate
+                         hours:(NSNumber *)hours;
+
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- TimeTracker/Model/FGTTimedTask.m
+++ TimeTracker/Model/FGTTimedTask.m
@@ -10,4 +10,23 @@
 
 @implementation FGTTimedTask
 
+
+- (instancetype)initWithClient:(NSString *)client
+                       summary:(NSString *)summary
+                          rate:(NSNumber *)rate
+                         hours:(NSNumber *)hours{
+    self = [super init];
+    
+    if(self){
+        _client = client;
+        _summary = summary;
+        _rate = rate;
+        _hours = hours;
+    }
+    
+    return self;
+}
+
+
+
 @end
