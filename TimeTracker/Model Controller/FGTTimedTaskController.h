c83b19e6c904ca3dd493330836630c71360bd52c
fritzgt <tuanis@live.com>
2020-05-13 11:03:55 +0300

    implemented method to add a new task to array

--- TimeTracker/Model Controller/FGTTimedTaskController.h
+++ TimeTracker/Model Controller/FGTTimedTaskController.h
@@ -8,9 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+@class FGTTimedTask;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface FGTTimedTaskController : NSObject
+
+//Properties
+@property (readonly, nonatomic) NSMutableArray<FGTTimedTask *> *timeTasks;
+
+//Methods
+- (void)createTimedTaskWith:(NSString *)client summary:(NSString *)summary rate:(double)rate hours:(double)hours;
 
 @end
 
--- TimeTracker/Model Controller/FGTTimedTaskController.m
+++ TimeTracker/Model Controller/FGTTimedTaskController.m
@@ -7,7 +7,26 @@
 //
 
 #import "FGTTimedTaskController.h"
+//import to create a new instace of the object and append to array
+#import "FGTTimedTask.h"
 
 @implementation FGTTimedTaskController
+
+- (instancetype)initWithTimeTasks:(NSMutableArray *)timeTasks {
+    self = [super init];
+    if(self){
+        _timeTasks = timeTasks;
+    };
+    
+    return self;
+}
+
+- (void)createTimedTaskWith:(NSString *)client summary:(NSString *)summary rate:(double)rate hours:(double)hours {
+    //1.Create a new instace
+    FGTTimedTask *TimedTask = [[FGTTimedTask alloc] initWithClient:client summary:summary rate:rate hours:hours];
+    //2. Add to array
+    [_timeTasks addObject:TimedTask];
+}
+
 
 @end
